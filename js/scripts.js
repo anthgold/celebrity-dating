6ead1e675a79405d80d5e07563259fd32a7b4b22
Anthony Gold and Tommy Jones <tommy@>
2016-03-24 13:37:16 -0700

    adds comments to javascript score tallier

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,19 @@
 $(document).ready(function() {
   $("form").submit(function(event) {
-
+    // Set starting score to 0
     var score = 0;
-
+    // Tallies the point value from each quiz question
     var result = parseInt($("input:radio[name=flavor]:checked").val());
-    score += result;
+    score += result; // Re-sets score to score + result
     result = parseInt($("input:radio[name=food]:checked").val());
-    score += result;
+    score += result; // Re-sets score to score + result
     result = parseInt($("input:radio[name=date]:checked").val());
-    score += result;
+    score += result; // Re-sets score to score + result
     result = parseInt($("input:radio[name=pet]:checked").val());
-    score += result;
-
-    // calculate results //
+    score += result; // Re-sets score to score + result
+    // Calculate results //
     if (score > 120) {
-      alert("Selma Hayek");
+      alert("Salma Hayek");
     }
     else if (score > 80) {
       alert("Angelina Jolie");
